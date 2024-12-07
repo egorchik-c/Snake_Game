9ec93aeca52c45b30d52b442ae2fa887862d1234
Егор Чернов <chegor47@bk.ru>
2024-12-07 16:34:03 +0300

    Голова на поле

--- snake.asm
+++ snake.asm
@@ -1,30 +1,43 @@
 section .data
-	field db "#########################", 0xa
-		db "#                       #", 0xa
-		db "#                       #", 0xa
-		db "#                       #", 0xa
-		db "#                       #", 0xa
-		db "#                       #", 0xa
-		db "#                       #", 0xa
-		db "#                       #", 0xa
-		db "#                       #", 0xa
-		db "#                       #", 0xa
-		db "#                       #", 0xa
-		db "#                       #", 0xa
-		db "#                       #", 0xa
-		db "#########################", 0xa
-	field_len equ $ - field
-	
+ field db "#######################", 0xa
+  db "#                     #", 0xa
+  db "#                     #", 0xa
+  db "#                     #", 0xa
+  db "#                     #", 0xa
+  db "#                     #", 0xa
+  db "#                     #", 0xa
+  db "#                     #", 0xa
+  db "#                     #", 0xa
+  db "#                     #", 0xa
+  db "#                     #", 0xa
+  db "#                     #", 0xa
+  db "#######################", 0xa
+ field_len equ $ - field
+ 
+ snake_x db 12
+ snake_y db 6
+ LEN db 24
+ 
 section .text
-	global _start
-	
+ global _start
+ 
 _start:
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, field
-	mov edx, field_len
-	int 0x80
-	
-	mov eax, 1
-	xor ebx, ebx
-	int 0x80
+ movzx eax, byte [snake_y]
+ movzx ebx, byte [LEN]
+ mul ebx
+ movzx edi, byte [snake_x]
+ add eax, edi
+ 
+ lea esi, byte [field]
+ add esi, eax
+ mov byte [esi], "O" 
+
+ mov eax, 4
+ mov ebx, 1
+ mov ecx, field
+ mov edx, field_len
+ int 0x80
+ 
+ mov eax, 1
+ xor ebx, ebx
+ int 0x80
